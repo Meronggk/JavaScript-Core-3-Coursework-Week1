43b28a8bf77ee4c9cf0442951590400b3e1ececb
Marryggk <99277162+Meronggk@users.noreply.github.com>
2023-08-23 20:39:11 +0100

    trial output

---
+++ index.js
@@ -0,0 +1,11 @@
+readingList = [
+    "The Count of Monte Cristo",
+    "East of Eden",
+    "Don Quixote",
+    "Pride and Predjudice",
+    "Flowers for Algernon",
+    ];
+    
+    readingList.shift();
+    
+    console.log(readingList);
